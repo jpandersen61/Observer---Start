0c8394436ae3d9e60bbb3f379101cb5cfc15e135
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-10-03 00:16:09 +0200

    Added testing to Main

--- Observer - Start/Program.cs
+++ Observer - Start/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Observer___Start
+namespace PizzaApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PizzaOven pizzaOven = new PizzaOven();
+            PizzaMan pizzaMan = new PizzaMan(pizzaOven);
+
+            Console.WriteLine("Pizza order: Ham and Bacon");
+            string[] order = { "Ham", "Bacon" };
+            pizzaMan.TakeOrder(order);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pizza order: Ham and Pepperoni");
+            string[] order2 = { "Ham", "Pepperoni" };
+            pizzaMan.TakeOrder(order2);
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
         }
     }
 }
